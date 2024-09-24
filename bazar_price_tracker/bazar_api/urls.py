0e8d5e1ef8916cb4f36d802505d957826715b884
Md Masipul Islam Siam <masipulislam@gmail.com>
2024-09-25 00:42:22 +0600

    search and profile page created

--- bazar_price_tracker/bazar_api/urls.py
+++ bazar_price_tracker/bazar_api/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('Search_results', views.Search_results, name='search_results'),
+    path('profile/', views.profile_view, name='profile'),
 ]
--- bazar_price_tracker/bazar_api/views.py
+++ bazar_price_tracker/bazar_api/views.py
@@ -1,8 +1,8 @@
 import requests
 from django.shortcuts import render
 
-def home(request):
-
+def fetch_products():
+    # Fetch data from the API
     prices_response = requests.get('https://bazar-price-tracker-api.onrender.com/api/prices/') 
     prices = prices_response.json() 
     
@@ -12,7 +12,7 @@ def home(request):
     markets_response = requests.get('https://bazar-price-tracker-api.onrender.com/api/markets/') 
     markets = markets_response.json() 
     
-    
+    # Combine the data into products
     products = []
     for price in prices:
         product = {}
@@ -23,7 +23,46 @@ def home(request):
         for market in markets:
             if market['id'] == price['market']:  
                 product['market'] = market['name']
-        if product not in products:
-            products.append(product)      
+        
+        if not product in products:
+            products.append(product)
+    
+    return products
 
-    return render(request, 'home.html', {'products' : products})
+def home(request):
+    # Fetch fresh products data
+    products = fetch_products()
+    return render(request, 'home.html', {'products': products})
+
+def Search_results(request):
+    # Fetch fresh products data
+    products = fetch_products()
+
+    # Get search parameters
+    market_name = request.GET.get('market_name', '').lower()
+    item_name = request.GET.get('item_name', '').lower()
+    
+    results = []
+     
+    # Search in the products list
+    for product in products:
+        product_market_name = product['market'].lower()
+        product_item_name = product['name'].lower()
+
+        # Check if the search terms match
+        if market_name and not item_name:
+            if market_name in product_market_name:
+                results.append(product)
+        elif item_name and not market_name:
+            if item_name in product_item_name:
+                results.append(product)
+        else:
+            if market_name in product_market_name:
+                if item_name in product_item_name:
+                    results.append(product)
+    
+    return render(request, 'search_results.html', {'results': results})
+
+def profile_view(request):
+    
+    return render(request, 'profile.html',)
